89e37f5ff2921d1be70187d5f304edb7ab968140
MarianoA18 <mariano.arce.n@gmail.com>
2026-07-01 03:56:44 +0000

    Agregar interfaz del stack

---
+++ include/stack.h
@@ -0,0 +1,20 @@
+#ifndef STACK_H
+#define STACK_H
+
+typedef struct StackNode {
+    int data;
+    struct StackNode *next;
+} StackNode;
+
+typedef struct {
+    StackNode *top;
+} Stack;
+
+Stack *stack_create(void);
+void stack_push(Stack *stack, int value);
+int stack_pop(Stack *stack, int *out_value);
+int stack_peek(const Stack *stack, int *out_value);
+int stack_is_empty(const Stack *stack);
+void stack_destroy(Stack *stack);
+
+#endif
