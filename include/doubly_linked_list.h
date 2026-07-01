8691a75356e086054b919224edf3f5f0414c08e3
MarianoA18 <mariano.arce.n@gmail.com>
2026-07-01 03:41:08 +0000

    Agregar interfaz de la lista doblemente enlazada

---
+++ include/doubly_linked_list.h
@@ -0,0 +1,27 @@
+#ifndef DOUBLY_LINKED_LIST_H
+#define DOUBLY_LINKED_LIST_H
+
+#include <stddef.h>
+
+typedef struct DNode {
+    int data;
+    struct DNode *prev;
+    struct DNode *next;
+} DNode;
+
+typedef struct {
+    DNode *head;
+    DNode *tail;
+} DoublyLinkedList;
+
+DoublyLinkedList *dll_create(void);
+void dll_insert_front(DoublyLinkedList *list, int value);
+void dll_insert_back(DoublyLinkedList *list, int value);
+void dll_insert_at(DoublyLinkedList *list, size_t position, int value);
+int dll_remove(DoublyLinkedList *list, int value);
+int dll_search(const DoublyLinkedList *list, int value);
+void dll_print_forward(const DoublyLinkedList *list);
+void dll_print_backward(const DoublyLinkedList *list);
+void dll_destroy(DoublyLinkedList *list);
+
+#endif
